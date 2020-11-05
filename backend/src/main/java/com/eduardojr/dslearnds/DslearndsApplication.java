00c6cdc691a2691ffbde48282dab7de901094ce9
eduardomsj <eduardomsj@gmail.com>
2020-11-05 01:06:43 -0300

    Project created

---
+++ backend/src/main/java/com/eduardojr/dslearnds/DslearndsApplication.java
@@ -0,0 +1,13 @@
+package com.eduardojr.dslearnds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearndsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearndsApplication.class, args);
+	}
+
+}
